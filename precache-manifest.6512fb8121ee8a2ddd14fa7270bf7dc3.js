d3a49a4456cf269c8849fb1930f1a20b59956174
Sujai123 <sujaibeniksj2020@srishakthi.ac.in>
2019-07-10 23:05:52 +0530

    Updates

---
+++ precache-manifest.6512fb8121ee8a2ddd14fa7270bf7dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e54715db0cfaa40e9a02",
+    "url": "/robofriends1/static/js/runtime~main.c8c46059.js"
+  },
+  {
+    "revision": "12f7aaca2a0c09e21472",
+    "url": "/robofriends1/static/js/main.a0a5c5f0.chunk.js"
+  },
+  {
+    "revision": "25567dae6cc97bc203e4",
+    "url": "/robofriends1/static/js/2.2d717d87.chunk.js"
+  },
+  {
+    "revision": "12f7aaca2a0c09e21472",
+    "url": "/robofriends1/static/css/main.a2da3a54.chunk.css"
+  },
+  {
+    "revision": "25567dae6cc97bc203e4",
+    "url": "/robofriends1/static/css/2.92c055b2.chunk.css"
+  },
+  {
+    "revision": "5c0e23bed539a2faa0312f8c07d543bb",
+    "url": "/robofriends1/index.html"
+  }
+];
